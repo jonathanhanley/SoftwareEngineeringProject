8bef361f966d57c446925c93a873ae0d6a02dac0
Karol <00name0010@gmail.com>
2019-11-14 09:22:45 +0000

    Use pointers and CharacterType in tests from unittest_tokenizer

--- Test_Suite/unittest_common.c
+++ Test_Suite/unittest_common.c
@@ -14,28 +14,29 @@ Liam de la Cour
 Include statements
 */
 #include <ctap.h>
+#include "common.h"
 
 /*
 Testing resetting the structure
 Creating test cases - list of inputs
-checking setting/resetting output_type.content
-checking setting/resetting  output_type.type
-checking setting/resetting  output_type.pointer
+checking setting/resetting output_type->content
+checking setting/resetting  output_type->type
+checking setting/resetting  output_type->pointer
 */
 void test_reset_struct(){
-        struct Output_type output_type;
+        CharacterType *output_type = newCharacterType();
         char inputs[] = {'L', 'R', ')', '(', 'O'};
         for (int i = 0; i < 5; i++){
-            output_type.type = inputs[i];
-            output_type.content[0] = inputs[i];
-            output_type.pointer = 1;
-            ok(strlen(output_type.content) > 0, "output_type.content has set");
-            ok(output_type.type == inputs[i], "output_type.type has been set");
-            ok(output_type.pointer != 0, "output_type.pointer has been increased");
-            output_type = reset_struct(output_type);
-            ok(strlen(output_type.content) == 0, "output_type.content has been reset");
-            ok(output_type.type == 'I', "output_type.type has been reset");
-            ok(output_type.pointer == 0, "output_type.pointer has been reset");
+            output_type->type = i;
+            output_type->content[0] = inputs[i];
+            output_type->pointer = 1;
+            ok(strlen(output_type->content) > 0, "output_type->content has set");
+            ok(output_type->type == i, "output_type->type has been set");
+            ok(output_type->pointer != 0, "output_type->pointer has been increased");
+            resetCharacterType(output_type);
+            ok(strlen(output_type->content) == 0, "output_type->content has been reset");
+            ok(output_type->type == 1, "output_type->type has been reset");
+            ok(output_type->pointer == 0, "output_type->pointer has been reset");
 
         }
 }
@@ -47,33 +48,37 @@ Checks the file to see if the structs have been written to the
 file correctly
 */
 void test_write_item_to_file(){
-    struct Output_type output_type;
-    rest_output_files();
-    output_type.type = 'I';
-    output_type.content[0] = '1';
-    output_type.pointer = 1;
-    output_type.content[1] = '0';
-    output_type.pointer++;
-    write_item_to_file(output_type);
-    output_type = reset_struct(output_type);
+    CharacterType *output_type = newCharacterType();
+    FILE *outFile;
+    outFile = fopen("tokenized.txt", "w");
+    char *keys[] = {"F", "I", "O", "L", "R"};
+    output_type->type = 1;
+    output_type->content[0] = '1';
+    output_type->pointer = 1;
+    output_type->content[1] = '0';
+    output_type->pointer++;
+    resultToFile(outFile, output_type, keys);
+    resetCharacterType(output_type);
 
-    output_type.type = 'O';
-    output_type.content[0] = '+';
-    output_type.pointer++;
-    write_item_to_file(output_type);
-    output_type = reset_struct(output_type);
+    output_type->type = 2;
+    output_type->content[0] = '+';
+    output_type->pointer++;
+    resultToFile(outFile, output_type, keys);
+    resetCharacterType(output_type);
 
-    output_type.type = 'L';
-    output_type.content[0] = '(';
-    output_type.pointer++;
-    write_item_to_file(output_type);
-    output_type = reset_struct(output_type);
+    output_type->type = 3;
+    output_type->content[0] = '(';
+    output_type->pointer++;
+    resultToFile(outFile, output_type, keys);
+    resetCharacterType(output_type);
 
-    output_type.type = 'R';
-    output_type.content[0] = ')';
-    output_type.pointer++;
-    write_item_to_file(output_type);
-    output_type = reset_struct(output_type);
+    output_type->type = 4;
+    output_type->content[0] = ')';
+    output_type->pointer++;
+    resultToFile(outFile, output_type, keys);
+    resetCharacterType(output_type);
+
+    fclose(outFile);
 
     FILE * file;
     char * line;
