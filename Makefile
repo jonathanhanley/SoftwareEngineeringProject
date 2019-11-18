5526b012a64cfd03e24cd112e1fb6a13a3fe8cca
Karol <00name0010@gmail.com>
2019-11-18 12:33:24 +0000

    Add 'make test' 'make integration_test' and 'make check' commands to Makefile. Update test_main.c to test all test files using prove

--- Makefile
+++ Makefile
@@ -4,10 +4,9 @@ LIST = stack common virtualmachine codegenerator infixtopostfix tokenizer
 objects = $(addsuffix .o, $(LIST))
 test_targets = $(addprefix test_, $(LIST))
 
-
 calculator:  $(objects) main.o
 			@echo "Building calculator"
-			$(CC) -o calculator $(objects) main.o
+			$(CC) -o calculator $^
 
 all: $(test_targets) $(LIST)
 
@@ -21,10 +20,20 @@ $(test_targets): test_%: %.o $(deps)
 unittest: $(test_targets)
 	$(CC) -o unittest_calculator Test_Suite/unittest_main.c
 
+integration_test: infixtopostfix.o tokenizer.o codegenerator.o $(deps)
+	$(CC) -o test_tokenizer_infixtopostfix.t Test_Suite/integrationtest_tokenizer_infixtopostfix.c $^
+	$(CC) -o test_infixtopostfix_codegenerator.t Test_Suite/integrationtest_infixtopostfix_codegenerator.c $^
+
+
 test_system: $(objects)
 	$(CC) -o test_system.t $(objects) Test_Suite/test_system.c
 
+test: $(test_targets) integration_test test_system
+		$(CC) -o test_calculator test_main.c
 
-.PHONY: clean
+.PHONY: clean check
 clean:
 	@rm -f unittest_calculator calculator *.o *.t $(LIST)
+
+check: test
+	./test_calculator
--- test_main.c
+++ test_main.c
@@ -1,46 +1,51 @@
 /*
 CS3500 - Software Engineering Project
 
-test_main.c
+Main file that runs all the other unittests together
 
-Karol Przestrzelski
 Colin Kelleher
 Jonathan Hanley
+Karol Przestrzelski
 Liam de la Cour
 */
 
-#include <ctap.h>
+/*
+Include statements
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include "codegenerator.h"
-#include "infixtopostfix.h"
-#include "stack.h"
-#include "virtualmachine.h"
-#include "tokenizer.h"
+/*
+listing all the test files below
+*/
+char *files[] = {
+    "test_codegenerator.t",
+    "test_common.t",
+    "test_infixtopostfix.t",
+    "test_stack.t",
+    "test_tokenizer.t",
+    "test_virtualmachine.t",
+    "test_tokenizer_infixtopostfix.t",
+    "test_infixtopostfix_codegenerator.t",
+    "test_system.t"
+};
 
-
-void runCodeGeneratorTests(){
-}
-
-void runInfixToPostfixTests(){
-}
-
-void runStackTests(){
-}
-
-void runVirtualMachineTests(){
-}
-
-void runTokenizerTests(){
-}
-
-void runOverallTests(){
-}
-
-TESTS {
-    runCodeGeneratorTests();
-    runInfixToPostfixTests();
-    runStackTests();
-    runVirtualMachineTests();
-    runTokenizerTests();
-    runOverallTests();
+/*
+Checking if the files exist and then executing them
+according to terminal commands
+*/
+int main(){
+    char cmd[150] = "prove";
+    for (int i=0; i<sizeof(files)/sizeof(*files); i++){
+        // Check if this file exists
+        FILE *f;
+        f = fopen(files[i], "r");
+        printf("%s\n", files[i]);
+        if (f == NULL) continue;
+        // Execute ./test_file
+        printf("Found tests for %s\n", files[i]);
+        sprintf(cmd, "%s ./%s", cmd, files[i]);
+    }
+    system(cmd);
 }
